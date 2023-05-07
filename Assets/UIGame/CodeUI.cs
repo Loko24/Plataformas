abefd2b554cb939954e2e4519453fdb233bbac11
daniel374 <37201418+daniel374@users.noreply.github.com>
2023-05-07 12:45:16 -0500

    Update main menu UI with custom Audio and UI

--- Assets/UIGame/CodeUI.cs
+++ Assets/UIGame/CodeUI.cs
@@ -4,10 +4,13 @@ using Unity.VisualScripting;
 using UnityEngine;
 //using UnityEngine.UI;
 using UnityEngine.UIElements;
+using static UnityEditor.Progress;
 
 public class CodeUI : MonoBehaviour
 {
     UIDocument _miMenu;
+    //Custom Audio
+    public AudioSource audioSourceGm;
     //main_menu
     Button _play_btn;
     Button _audio_opt_btn;
@@ -15,13 +18,22 @@ public class CodeUI : MonoBehaviour
     Button _exit_btn;
     //second_opt_menu
     VisualElement _right;
+    VisualElement _options;
     VisualElement _sound_opt;
     VisualElement _credits;
     Button _bkmenu;
     Toggle _onoff_sound;
     DropdownField _songs_list;
-    Slider _audio_volumen;
+    Slider _audio_volume;
 
+    /*private void Start()
+    {
+        audioSourceGm = GetComponent<AudioSource>();
+        _onoff_sound.value = audioSourceGm.enabled;
+        _audio_volume.value = audioSourceGm.volume;
+        _credits.SetEnabled(false);
+        _sound_opt.SetEnabled(false);
+    }*/
 
     private void OnEnable()
     {
@@ -34,13 +46,21 @@ public class CodeUI : MonoBehaviour
         _exit_btn = root.Q<Button>("exitBtn");
         
         _right = root.Q<VisualElement>("right");
+        _options = root.Q<VisualElement>("options");
         _sound_opt = root.Q<VisualElement>("sound_opt");
         _credits = root.Q<VisualElement>("credits");
         
         _bkmenu = root.Q<Button>("bkmenu");
         _onoff_sound = root.Q<Toggle>("onoff_sound");
         _songs_list = root.Q<DropdownField>("songs-list");
-        _audio_volumen = root.Q<Slider>("volumen");
+        _audio_volume = root.Q<Slider>("volume");
+        //Custom Audio
+        audioSourceGm = GetComponent<AudioSource>();
+        _onoff_sound.value = audioSourceGm.enabled;
+        _audio_volume.value = audioSourceGm.volume;
+        //
+        _credits.SetEnabled(false);
+        _sound_opt.SetEnabled(false);
         //Callbacks
         _play_btn.RegisterCallback<ClickEvent, int>(OpenOptions, 1);
         _audio_opt_btn.RegisterCallback<ClickEvent, int>(OpenOptions, 2);
@@ -49,7 +69,7 @@ public class CodeUI : MonoBehaviour
 
         _onoff_sound.RegisterValueChangedCallback(OnOffSound);
         _songs_list.RegisterValueChangedCallback(MySongsList);
-        _audio_volumen.RegisterValueChangedCallback(AudioVolume);
+        _audio_volume.RegisterValueChangedCallback(AudioVolume);
         _bkmenu.RegisterCallback<ClickEvent>(CloseOptMenu);
 
     }
@@ -65,17 +85,25 @@ public class CodeUI : MonoBehaviour
                 break;
             case 2:
                 Debug.Log("OPTION menu sound_opt" + opt);
-                _credits.RemoveFromClassList(".menu_opt_active");
+                _credits.SetEnabled(false);
+                _credits.RemoveFromHierarchy();
+                _sound_opt.SetEnabled(true);
+                _options.Add(_sound_opt);
+                /*_credits.RemoveFromClassList(".menu_opt_active");
                 _sound_opt.RemoveFromClassList(".menu_opt_deactive");
                 _credits.AddToClassList(".menu_opt_deactive");
-                _sound_opt.AddToClassList(".menu_opt_active");
+                _sound_opt.AddToClassList(".menu_opt_active");*/
                 break; 
             case 3:
+                _credits.SetEnabled(true);
+                _sound_opt.SetEnabled(false);
+                _sound_opt.RemoveFromHierarchy();
+                _options.Add(_credits);
                 Debug.Log("OPTION menu credits" + opt);
-                _sound_opt.RemoveFromClassList(".menu_opt_active");
+                /*_sound_opt.RemoveFromClassList(".menu_opt_active");
                 _credits.RemoveFromClassList(".menu_opt_deactive");
                 _sound_opt.AddToClassList(".menu_opt_deactive");
-                _credits.AddToClassList(".menu_opt_active");
+                _credits.AddToClassList(".menu_opt_active");*/
                 break; 
             case 4:
                 break;
@@ -83,6 +111,7 @@ public class CodeUI : MonoBehaviour
     }
     void OnOffSound(ChangeEvent<bool> evt)
     {
+        audioSourceGm.enabled = evt.newValue;
         Debug.Log("HandleCallback invoke value " + evt.newValue);
     }
 
@@ -93,7 +122,7 @@ public class CodeUI : MonoBehaviour
 
     void AudioVolume(ChangeEvent<float> evt)
     {
-
+        audioSourceGm.volume = evt.newValue;
     }
 
     void CloseOptMenu(ClickEvent evt)
