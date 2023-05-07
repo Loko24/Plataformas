18afcef441800764f85ee65b495b3280ba4eccb6
daniel374 <37201418+daniel374@users.noreply.github.com>
2023-05-07 10:44:37 -0500

    Update code UI menu

--- Assets/UIGame/CodeUI.cs
+++ Assets/UIGame/CodeUI.cs
@@ -7,102 +7,98 @@ using UnityEngine.UIElements;
 
 public class CodeUI : MonoBehaviour
 {
-    UIDocument miMenu;
+    UIDocument _miMenu;
     //main_menu
-    Button play_btn;
-    Button audio_opt_btn;
-    Button credits_btn;
-    Button exit_btn;
+    Button _play_btn;
+    Button _audio_opt_btn;
+    Button _credits_btn;
+    Button _exit_btn;
     //second_opt_menu
-    VisualElement right;
-    VisualElement sound_opt;
-    VisualElement credits;
-    Button bkmenu;
-    Toggle onoff_sound;
-    DropdownField songs_list;
-    Slider audio_volumen;
+    VisualElement _right;
+    VisualElement _sound_opt;
+    VisualElement _credits;
+    Button _bkmenu;
+    Toggle _onoff_sound;
+    DropdownField _songs_list;
+    Slider _audio_volumen;
 
 
     private void OnEnable()
     {
-        miMenu = GetComponent<UIDocument>();
-        VisualElement root = miMenu.rootVisualElement;
+        _miMenu = GetComponent<UIDocument>();
+        VisualElement root = _miMenu.rootVisualElement;
         //main_opt
-        play_btn = root.Q<Button>("playBtn");
-        audio_opt_btn = root.Q<Button>("optionsBtn");
-        credits_btn = root.Q<Button>("creditsBtn");
-        exit_btn = root.Q<Button>("exitBtn");
+        _play_btn = root.Q<Button>("playBtn");
+        _audio_opt_btn = root.Q<Button>("optionsBtn");
+        _credits_btn = root.Q<Button>("creditsBtn");
+        _exit_btn = root.Q<Button>("exitBtn");
         
-        right = root.Q<VisualElement>("right");
-        sound_opt = root.Q<VisualElement>("sound_opt");
-        credits = root.Q<VisualElement>("credits");
+        _right = root.Q<VisualElement>("right");
+        _sound_opt = root.Q<VisualElement>("sound_opt");
+        _credits = root.Q<VisualElement>("credits");
         
-        bkmenu = root.Q<Button>("bkmenu");
-        //onoff_sound = GetComponent<Toggle>();
-        onoff_sound = root.Q<Toggle>("onoff_sound");
-        songs_list = root.Q<DropdownField>("songs-list");
-        audio_volumen = root.Q<Slider>("volumen");
+        _bkmenu = root.Q<Button>("bkmenu");
+        _onoff_sound = root.Q<Toggle>("onoff_sound");
+        _songs_list = root.Q<DropdownField>("songs-list");
+        _audio_volumen = root.Q<Slider>("volumen");
         //Callbacks
-        play_btn.RegisterCallback<ClickEvent, int>(openOptions, 1);
-        audio_opt_btn.RegisterCallback<ClickEvent, int>(openOptions, 2);
-        credits_btn.RegisterCallback<ClickEvent, int>(openOptions, 3);
-        exit_btn.RegisterCallback<ClickEvent, int>(openOptions, 4);
+        _play_btn.RegisterCallback<ClickEvent, int>(OpenOptions, 1);
+        _audio_opt_btn.RegisterCallback<ClickEvent, int>(OpenOptions, 2);
+        _credits_btn.RegisterCallback<ClickEvent, int>(OpenOptions, 3);
+        _exit_btn.RegisterCallback<ClickEvent, int>(OpenOptions, 4);
 
-        /*onoff_sound.onValueChanged.AddListener(delegate {
-            onOffSound(onoff_sound);
-        });*/
-        onoff_sound.RegisterValueChangedCallback(onOffSound);
-        songs_list.RegisterValueChangedCallback(mySongsList);
-        audio_volumen.RegisterValueChangedCallback(audioVolumen);
-        bkmenu.RegisterCallback<ClickEvent>(closeOptMenu);
+        _onoff_sound.RegisterValueChangedCallback(OnOffSound);
+        _songs_list.RegisterValueChangedCallback(MySongsList);
+        _audio_volumen.RegisterValueChangedCallback(AudioVolume);
+        _bkmenu.RegisterCallback<ClickEvent>(CloseOptMenu);
 
     }
 
-    void openOptions(ClickEvent evt, int opt)
+    void OpenOptions(ClickEvent evt, int opt)
     {
 
-        right.RemoveFromClassList("rightcont_active");
-        right.AddToClassList("rightcont_active");
+        _right.RemoveFromClassList("rightcont_active");
+        _right.AddToClassList("rightcont_active");
         switch (opt)
         {
             case 1:
                 break;
             case 2:
                 Debug.Log("OPTION menu sound_opt" + opt);
-                credits.RemoveFromClassList(".menu_opt_active");
-                sound_opt.RemoveFromClassList(".menu_opt_deactive");
-                credits.AddToClassList(".menu_opt_deactive");
-                sound_opt.AddToClassList(".menu_opt_active");
+                _credits.RemoveFromClassList(".menu_opt_active");
+                _sound_opt.RemoveFromClassList(".menu_opt_deactive");
+                _credits.AddToClassList(".menu_opt_deactive");
+                _sound_opt.AddToClassList(".menu_opt_active");
                 break; 
             case 3:
                 Debug.Log("OPTION menu credits" + opt);
-                sound_opt.RemoveFromClassList(".menu_opt_active");
-                credits.RemoveFromClassList(".menu_opt_deactive");
-                sound_opt.AddToClassList(".menu_opt_deactive");
-                credits.AddToClassList(".menu_opt_active");
+                _sound_opt.RemoveFromClassList(".menu_opt_active");
+                _credits.RemoveFromClassList(".menu_opt_deactive");
+                _sound_opt.AddToClassList(".menu_opt_deactive");
+                _credits.AddToClassList(".menu_opt_active");
                 break; 
             case 4:
                 break;
         }
     }
-    void onOffSound(ChangeEvent<bool> evt)
+    void OnOffSound(ChangeEvent<bool> evt)
     {
         Debug.Log("HandleCallback invoke value " + evt.newValue);
     }
 
-    void mySongsList(ChangeEvent<string> evt)
+    void MySongsList(ChangeEvent<string> evt)
     {
 
     }
 
-    void audioVolumen(ChangeEvent<float> evt)
+    void AudioVolume(ChangeEvent<float> evt)
     {
 
     }
 
-    void closeOptMenu(ClickEvent evt)
+    void CloseOptMenu(ClickEvent evt)
     {
-        right.RemoveFromClassList("rightcont_active");
+        _right.RemoveFromClassList("rightcont_active");
     }
 
 
