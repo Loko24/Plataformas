d6dae41f213f89aeca6e9cbd080bab2b159a1802
daniel374 <37201418+daniel374@users.noreply.github.com>
2023-05-08 09:39:47 -0500

    adjust slider customAudio volume and solution to error in codeUI script

--- Assets/UIGame/CodeUI.cs
+++ Assets/UIGame/CodeUI.cs
@@ -1,12 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 //using UnityEngine.UI;
 using UnityEngine.UIElements;
-using static UnityEditor.Progress;
 
 public class CodeUI : CustomAudioGame
 {
